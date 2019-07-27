879411e79f1823366ccab2bb9a6b48fd505523b0
julia smith <julia.smith@us.sogeti.com>
2019-07-26 21:44:12 -0500

    Schemas SQL Commit
    Schemas SQL

---
+++ SQL Homework/schemas.sql
@@ -0,0 +1,64 @@
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
+   emp_no INT NOT NULL,
+   birth_date DATE NOT NULL,
+   first_name VARCHAR(40) NOT NULL,
+   last_name VARCHAR(40) NOT NULL,
+   gender VARCHAR(1) NOT NULL,
+   hire_date DATE NOT NULL,
+   PRIMARY KEY (emp_no)
+);
+
+DROP TABLE IF EXISTS dept_emp;
+CREATE TABLE dept_emp(
+emp_no INT NOT NULL,
+dept_no VARCHAR(4) NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+PRIMARY KEY (emp_no)
+);
+
+DROP TABLE IF EXISTS dept_manager;
+CREATE TABLE dept_manager(
+emp_no INT NOT NULL,
+dept_no VARCHAR(4) NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+PRIMARY KEY (emp_no)
+);
+
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments(
+dept_no VARCHAR(4) NOT NULL,
+dept_name VARCHAR(40),
+PRIMARY KEY (dept_no)
+);
+
+DROP TABLE IF EXISTS salaries;
+CREATE TABLE salaries(
+emp_no INT NOT NULL,
+salary INT NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+PRIMARY KEY (emp_no)
+);
+
+DROP TABLE IF EXISTS titles;
+CREATE TABLE titles(
+emp_no INT NOT NULL,
+title VARCHAR(30) NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+PRIMARY KEY (emp_no)
+);
+
+
+
+
+
+
+
+
+
+
+
